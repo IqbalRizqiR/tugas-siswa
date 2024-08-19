bf291b462744b09caf3ac08baa6bb87c3f944ec7
Iqbal Rizqi <rama.ramajr121@gmail.com>
2024-08-19 14:19:11 +0700

    first commit

---
+++ Siswa.java
@@ -0,0 +1,24 @@
+public class Siswa {
+    int id;
+    String nama;
+    double ipk;
+
+    public Siswa() {
+        id = 0;
+        nama = "null";
+        ipk = 0.0;
+    }
+
+    // Getter
+
+
+    // Setter
+
+    // Method
+
+    public void print() {
+        System.out.println("ID : " + id);
+        System.out.println("Nama : " + nama);
+        System.out.println("IPK : " + ipk);
+    }
+}
---
+++ SiswaTester.java
@@ -0,0 +1,43 @@
+public class SiswaTester {
+    public static void main(String[] args) {
+        Siswa arkan = new Siswa();
+        Siswa ito = new Siswa();
+        Siswa rabbani = new Siswa();
+        Siswa davin = new Siswa();
+        Siswa nafis = new Siswa();
+        arkan.id = 22;
+        arkan.nama = "Muhammad Arkan Mariadi";
+        arkan.ipk = 90.5;
+
+        nafis.id = 25;
+        nafis.nama = "Nafis Dwi Harits";
+        nafis.ipk = 87.5;
+
+        ito.id = 4;
+        ito.nama = "Anggito Abimanyu";
+        ito.ipk = 88.5;
+
+        davin.id = 8;
+        davin.nama = "Davin Falih Ramadhan";
+        davin.ipk = 91.5;
+        
+        rabbani.id = 23;
+        rabbani.nama = "Muhammad Rabbani Aryanta";
+        rabbani.ipk = 93.5;
+
+        arkan.print();
+        System.out.println("--------");
+        davin.print();
+        System.out.println("--------");
+        nafis.print();
+        System.out.println("--------");
+        rabbani.print();
+        System.out.println("--------");
+        ito.print();
+        System.out.println("--------");
+    }
+
+
+    // Constructor
+
+}
